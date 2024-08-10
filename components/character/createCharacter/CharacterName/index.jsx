f9abcb8ef2a758427813150b18977cfae17b8fed
DerPatron400 <alexksela@hotmail.com>
2024-08-10 18:48:37 +0200

    Revert some logic
    asf

--- components/character/createCharacter/CharacterName/index.jsx
+++ components/character/createCharacter/CharacterName/index.jsx
@@ -3,11 +3,8 @@ import React from "react";
 import CustomButton from "@/components/ui/custom-button";
 import { getRandomName } from "@/lib/Helpers/character";
 import useCharacterStore from "@/utils/characterStore";
-import Image from "next/image";
-
-export default function Index({ character, setCharacter }) {
+export default function index({ character, setCharacter }) {
   const { characterNameError, setCharacterNameError } = useCharacterStore();
-
   const handleRandomCharacterName = async () => {
     const name = getRandomName();
     setCharacter((prev) => ({
@@ -16,28 +13,24 @@ export default function Index({ character, setCharacter }) {
     }));
     setCharacterNameError(false);
   };
-
   return (
     <div className='flex h-screen fixed top-[156px] left-0 px-6 w-full flex-col gap-5'>
-      <div className='w-full h-[248px] rounded-[16px] overflow-hidden'>
-        <Image
-          src='/images/CreateCharacter/CharacterName/CharacterName.png'
-          alt='Character Name Background'
-          layout='fill' // Adjust based on your layout needs
-          objectFit='cover'
-        />
-      </div>
+      <img
+        src={`/images/CreateCharacter/CharacterName/CharacterName.png`}
+        alt=''
+        className='w-full h-[248px] rounded-[16px]  object-cover'
+      />
       <div className='flex flex-col gap-1'>
+        {" "}
         <CustomInput
           focusOnError={true}
           value={character.name}
           icon={
             character.name && (
-              <Image
+              <img
                 src='/Icons/Success.svg'
                 alt='Success'
-                width={16}
-                height={16}
+                className=' h-4 w-4'
               />
             )
           }
@@ -51,14 +44,14 @@ export default function Index({ character, setCharacter }) {
           className={"w-full"}
         />
         {characterNameError && (
-          <span className='text-errorRed uppercase font-roboto-mono text-[10px]'>
+          <span className='text-errorRed uppercase font-roboto-mono  text-[10px] '>
             Please enter a character name
           </span>
         )}
       </div>
 
       <CustomButton withIcon onClick={handleRandomCharacterName}>
-        <Image src='/Icons/Random.svg' alt='Random Character Name' width={20} height={20} />
+        <img src='/Icons/Random.svg' alt='logo' className='h-5 w-5 ' />
         RANDOM CHARACTER Name
       </CustomButton>
     </div>
