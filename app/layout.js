e1f8e11027d3555b094187f3c8d7c920c985f3a5
DerPatron400 <alexksela@hotmail.com>
2024-08-11 13:02:47 +0200

    changed root layout

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@
 import React, { useEffect, Suspense, memo } from "react";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navigation/Navbar";
-import GameplayNavbar from "@/components/navigation/GameplayNavbar";
 import Footer from "@/components/navigation/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import { usePathname } from "next/navigation";
@@ -11,7 +10,6 @@ import Script from "next/script";
 import "./globals.css";
 import useGameStore from "@/utils/gameStore";
 import CreditsDialogue from "@/components/ui/Shared/Dialogue/GetCredits";
-import { capitalizeFirstLetterOfEachWord } from "@/lib/Helpers/shared";
 import useStepperStore from "@/utils/characterStore";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,8 +19,8 @@ const MemoizedFooter = memo(Footer);
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-
   const { activeStep } = useStepperStore();
+
   const isTransparentNavbar =
     pathname.includes("/auth") || pathname.includes("/game/play");
   const showFooter =
@@ -44,32 +42,19 @@ export default function RootLayout({ children }) {
 
   useEffect(() => {
     const setDocumentTitle = (url) => {
-      let title = "DnD AI - Play AI-Guided Pen and Paper Games and create breathtaking images in the process"; // Default title
+      let title = "DnD AI - Play AI-Guided Pen and Paper Games";
 
       if (url.includes("/character/sheet")) {
         title = "DnD AI / Character Overview";
       } else if (url === "/") {
         title = "DnD AI / Home";
       } else if (url.includes("/campaign/")) {
-        if (url.includes("/campaign/create")) {
-          title = "DnD AI / Create Campaign";
-        } else if (url.includes("/campaign/my-campaigns")) {
-          title = "DnD AI / My Campaigns";
-        } else {
-          // Extract the campaign ID if needed or set a generic campaign details title
-          const campaignIdMatch = url.match(/\/campaign\/([^\/]+)/);
-          const campaignId = campaignIdMatch ? campaignIdMatch[1] : null;
-
-          if (campaignId) {
-            title = "DnD AI / Campaign Details";
-          } else {
-            title = "DnD AI / Campaign";
-          }
-        }
+        title = "DnD AI / Campaign Details";
       } else {
         const pageTitle = url.split("/").pop().replaceAll("-", " ");
-        title = `${capitalizeFirstLetterOfEachWord(pageTitle)} - DnD AI`;
+        title = `${pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1)} - DnD AI`;
       }
+
       document.title = title;
       window.gtag("config", "G-BTHMYX7TZ9", {
         page_title: title,
@@ -77,32 +62,23 @@ export default function RootLayout({ children }) {
         screen_name: title,
       });
     };
+
     setDocumentTitle(pathname);
-    // Re-run on path changes
     const handleRouteChange = (url) => {
       setDocumentTitle(url);
     };
     if (window.gtag) {
-      handleRouteChange(pathname); // Capture the initial page
+      handleRouteChange(pathname);
     }
   }, [pathname]);
-  
-  
+
   return (
     <html lang="en" suppressHydrationWarning className={inter.className}>
-      <GoogleOAuthProvider clientId="1036030324483-ltg0nqpg0ectr5q3n7cfa66l7eq1ban8.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId="your-client-id">
         <head>
-          {/* Google Analytics Script */}
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1"
-          />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
           <link rel="icon" href="/favicon.ico" />
-          <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=G-BTHMYX7TZ9`}
-            async
-          />
+          <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-BTHMYX7TZ9`} async />
           <Script
             id="google-analytics"
             strategy="afterInteractive"
@@ -118,46 +94,22 @@ export default function RootLayout({ children }) {
             }}
             async
           />
-          {/* Dynamic Meta Tag */}
-          <meta
-            name="description"
-            content="Join DnDAI to play DnD with AI. AI-supported pen and paper games. Explore adventure games, text-based games, and interactive fiction online for free."
-          />
-          <meta
-            name="keywords"
-            content="AI adventure games, text-based games, interactive fiction, role-playing games, free online adventure games, AI Game, Free online game 2024, Story writing game, Dungeons and Dragons with AI game, Images created with AI for free"
-          />
-          <meta
-            property="og:title"
-            content="DnD AI - Play an AI driven Story game and create breathtaking images in the process"
-          />
-          <meta
-            property="og:description"
-            content="Join DnDAI to play DnD with AI. AI-supported pen and paper games. Explore adventure games, text-based games, and interactive fiction online for free."
-          />
+          {/* Global Meta Tags */}
+          <meta name="description" content="Join DnDAI to play DnD with AI. AI-supported pen and paper games." />
+          <meta name="keywords" content="AI adventure games, text-based games, interactive fiction, role-playing games, free online adventure games, AI Game, Free online game 2024" />
+          <meta property="og:title" content="DnD AI - Play an AI driven Story game and create breathtaking images in the process" />
+          <meta property="og:description" content="Play DnD AI for free. Interactive Dungeons and Dragons Game." />
           <meta property="og:url" content="https://www.dndai.app" />
+          <meta property="og:image" content="https://dndai-images.s3.eu-central-1.amazonaws.com/Headers/Header.webp" />
           <meta property="og:type" content="website" />
         </head>
-        <body
-          className={`w-screen hide-scrollbar relative max-w-screen overflow-x-hidden bg-russianViolet`}
-        >
+        <body className="w-screen hide-scrollbar relative max-w-screen overflow-x-hidden bg-russianViolet">
           {showDiceGold && <div id="dice-box-gold" className="dice-box"></div>}
           {showDiceGame && <div id="dice-box-game" className="dice-box"></div>}
-          {showDiceAbilities && (
-            <div id="dice-box-abilities" className="dice-box"></div>
-          )}
-
-          <img
-            src="/images/bg.png"
-            alt="Background"
-            className="h-screen w-screen object-fill fixed top-0 left-0 z-0"
-          />
-          <MemoizedNavbar
-            characterSheet={characterSheet}
-            variant={isTransparentNavbar ? "transparent" : "glass"}
-          />
+          {showDiceAbilities && <div id="dice-box-abilities" className="dice-box"></div>}
+          <img src="/images/bg.png" alt="Background" className="h-screen w-screen object-fill fixed top-0 left-0 z-0" />
+          <MemoizedNavbar characterSheet={characterSheet} variant={isTransparentNavbar ? "transparent" : "glass"} />
           <main className="z-[1]">{children}</main>
-
           {showFooter && <MemoizedFooter />}
           <Suspense fallback={null}>
             <CreditsDialogue />
