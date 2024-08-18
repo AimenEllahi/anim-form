db29faa08aeb3925e31f179b8bddc839df301e76
DerPatron400 <alexksela@hotmail.com>
2024-08-18 13:45:26 +0200

    removed global metaTag fallback

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
 import PageClient from "./page.client";
 
-export const metadata = {
-  title: "DND AI", // This will be the default title if JavaScript is disabled
-};
-
 export default function Home() {
   return <PageClient />;
 }
