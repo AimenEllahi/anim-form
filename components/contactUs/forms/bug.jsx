91decd111b92e23854bb8bb84ccc448b7854647b
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-08-18 04:39:53 -0700

    revisions

--- components/contactUs/forms/bug.jsx
+++ components/contactUs/forms/bug.jsx
@@ -6,6 +6,7 @@ import CustomButton from "@/components/ui/custom-button";
 import ArrowRight from "@/components/ui/Icons/ArrowRight";
 import Upload from "./upload";
 
+//bug form
 export default function bug() {
   return (
     <div className=" flex w-full flex-col gap-6 justify-center items-center">
