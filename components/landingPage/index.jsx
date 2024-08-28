4780a0903055a1b8464a8eba561c5d539cab9d89
DerPatron400 <alexksela@hotmail.com>
2024-08-28 21:32:28 +0200

    made header Video much smaller in size

--- components/landingPage/index.jsx
+++ components/landingPage/index.jsx
@@ -59,7 +59,7 @@ export default function Home() {
               className='absolute top-0 left-0 w-full h-full object-cover'
               onError={handleVideoError}
             >
-              <source src='https://dndai-images.s3.eu-central-1.amazonaws.com/abilities/header.mp4' type='video/mp4' />
+              <source src='/videos/header.mp4' type='video/mp4' />
               Your browser does not support the video tag.
             </video>
           ) : (
