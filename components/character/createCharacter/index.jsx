90dd84ee1c44b2b4de318fd9279c206e5eaffa3f
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-08-30 13:30:02 -0700

    revisions

--- components/character/createCharacter/index.jsx
+++ components/character/createCharacter/index.jsx
@@ -1,4 +1,4 @@
-  "use client";
+"use client";
 import React, { useEffect, useState } from "react";
 import Create from "@/components/character/createCharacter/create";
 import useStepperStore from "@/utils/characterStore";
@@ -13,7 +13,7 @@ export default function Index() {
   const [character, setCharacter] = useState(INITIAL_CHARACTER);
 
   useEffect(() => {
-    //height 100 vh and not scrollable when show modal
+    //height 100 vh  not scrollable when show modal
     if (showModal) {
       document.body.style.overflowY = "hidden ";
     }
@@ -23,16 +23,16 @@ export default function Index() {
     };
   }, [showModal]);
   return (
-    <div className='h-full md:h-screen md:max-h-screen box-border  w-full flex flex-col bg-gradient pt-[136px] md:pt-[120px] px-6 lg:px-12  '>
+    <div className="h-full md:h-screen md:max-h-screen box-border  w-full flex flex-col bg-gradient pt-[136px] md:pt-[120px] px-6 lg:px-12  ">
       <ImageModal />
       {/* Desktop */}
-      <div className='hidden md:flex flex-col gap-2.5 '>
-        <h1 className='text-center flex justify-start text-white headline-3 z-[10] '>
+      <div className="hidden md:flex flex-col gap-2.5 ">
+        <h1 className="text-center flex justify-start text-white headline-3 z-[10] ">
           Create your character
         </h1>
       </div>
 
-      <div className='w-full flex z-[10] h-1/2 flex-1   '>
+      <div className="w-full flex z-[10] h-1/2 flex-1   ">
         <Create setCharacter={setCharacter} character={character} />
       </div>
       <BottomMenu setCharacter={setCharacter} character={character} />
