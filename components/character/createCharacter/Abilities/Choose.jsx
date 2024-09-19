2bcaaadb16c7f1650cb14e9b6fd5b963dfafb901
DerPatron400 <alexksela@hotmail.com>
2024-09-19 14:07:43 +0200

    added the infotext for ability roll

--- components/character/createCharacter/Abilities/Choose.jsx
+++ components/character/createCharacter/Abilities/Choose.jsx
@@ -271,7 +271,9 @@ export default function Choose({ abilities, handleChangeAbilities }) {
               </div>
             </TooltipTrigger>
             <TooltipContent>
-              <p>Add your message here</p>
+              < p style={{ marginBottom: '10px' }}>Here, you can roll for your ability scores. When you press the "Roll" button, four 6-sided dice will be rolled.</p>
+              < p style={{ marginBottom: '10px' }}>The lowest result will be dropped, and the remaining three will be added together to give you a total. You can then assign that total to an ability score.</p>  
+              < p style={{ marginBottom: '10px' }}> You also have the option to roll for all your ability scores at once by clicking "Roll All Dice." In this case, the scores will be randomly assigned to each ability, but you can swap them around if you'd like.</p>  
             </TooltipContent>
           </Tooltip>
         </TooltipProvider>
