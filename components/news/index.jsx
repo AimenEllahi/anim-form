8dd49536fdf7fda58f258f4a3f4098141ce1cea3
DerPatron400 <alexksela@hotmail.com>
2024-09-20 19:09:42 +0200

    added news page and changed padding top on most of pages to rem

---
+++ components/news/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function index() {
+    return (
+      <div className='h-full text-white w-full flex flex-col pt-[94px] md:pt-[9rem] px-5 lg:px-12 pb-32'>
+        <div className='flex flex-col w-full gap-10 z-[10]'>
+  
+          <div className=' text-white grid grid-cols-1 md:grid-cols-12  gap-8 justify-end items-end'>
+          </div>
+          <div className='flex flex-col gap-5 w-full mt-[24px] md:mt-[265px]'>
+            <span className='headline-3'>Our Team</span>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
