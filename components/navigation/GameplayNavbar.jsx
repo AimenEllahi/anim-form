4a504ad4faaf85722d8e4cc95809f1e09823c1ab
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-09-21 02:04:59 -0700

    revisions

--- components/navigation/GameplayNavbar.jsx
+++ components/navigation/GameplayNavbar.jsx
@@ -36,10 +36,11 @@ export default function GameplayNavbar({ loading, setLoading }) {
 
   const { setCurrentCharacter, game, currentCharacter } = useGameStore();
 
+  //saving character
   const handleSaveCharacter = async () => {
     try {
       setLoading(true);
-     // console.log(game);
+      // console.log(game);
       const payload = {
         characterId: game.characterId,
         gameId: game._id,
@@ -63,7 +64,7 @@ export default function GameplayNavbar({ loading, setLoading }) {
   };
 
   return (
-    <div className='px-5 md:px-8 fixed top-5 md:top-8 z-20 w-full'>
+    <div className="px-5 md:px-8 fixed top-5 md:top-8 z-20 w-full">
       <div
         className={cn(
           " running-text-mono w-full rounded-2xl bg-transparent border-0 top-0 md:top-8 left-0 translate-x-[0] flex h-auto md:h-[64px]  justify-center md:p-[8px] md:ps-4 "
@@ -75,59 +76,59 @@ export default function GameplayNavbar({ loading, setLoading }) {
           }
         >
           <Link
-            href='#'
-            className='text-white hover:text-gray1 transition-all duration-300 ease-in-out keychainify-checked'
+            href="#"
+            className="text-white hover:text-gray1 transition-all duration-300 ease-in-out keychainify-checked"
           >
             <img
-              src='/Icons/Logo.svg'
-              alt='logo'
-              className='h-8 object-contain'
+              src="/Icons/Logo.svg"
+              alt="logo"
+              className="h-8 object-contain"
             />
           </Link>
-          <div className='flex gap-5 items-center'>
+          <div className="flex gap-5 items-center">
             <CustomIcontext>
               <img
-                src='/gems/Mythic.webp'
-                alt='Mythic gem'
-                className='h-[18px] object-contain '
+                src="/gems/Mythic.webp"
+                alt="Mythic gem"
+                className="h-[18px] object-contain "
               />
               {user.blueCredits}
             </CustomIcontext>
             <CustomIcontext>
               <img
-                src='/gems/Legendary.webp'
-                alt='Legendary Gem'
-                className='h-[18px] object-contain '
+                src="/gems/Legendary.webp"
+                alt="Legendary Gem"
+                className="h-[18px] object-contain "
               />
               {user.yellowCredits}
             </CustomIcontext>
             <DropdownMenu>
-              <DropdownMenuTrigger className=' bg-blur  prevent-redirect  !h-9 !w-9 cursor-pointer !border ease-animate  border-white/10 hover:border-white/20 hover:bg-white/10 active:bg-white/20  active:border-white/40 disabled:opacity-30% disabled:pointer-events-none hover:!duration-200 !rounded-full active:!duration-100 !flex !items-center !justify-center'>
+              <DropdownMenuTrigger className=" bg-blur  prevent-redirect  !h-9 !w-9 cursor-pointer !border ease-animate  border-white/10 hover:border-white/20 hover:bg-white/10 active:bg-white/20  active:border-white/40 disabled:opacity-30% disabled:pointer-events-none hover:!duration-200 !rounded-full active:!duration-100 !flex !items-center !justify-center">
                 <MoreOptions
-                  className='w-5 h-5  prevent-redirect'
-                  fill='white'
+                  className="w-5 h-5  prevent-redirect"
+                  fill="white"
                 />
               </DropdownMenuTrigger>
-              <DropdownMenuContent className='bg-transparent uppercase flex flex-col mt-4 p-2 !px-[9px]  border border-white/10 z-[10] bg-blur menu-shadow text-white running-text-mono rounded-[16px] !gap-y-2'>
-                <DropdownMenuItem className='flex !p-0  !my-0 w-full focus:bg-transparent focus:text-white  transition-all duration-300 ease-linear cursor-pointer'>
+              <DropdownMenuContent className="bg-transparent uppercase flex flex-col mt-4 p-2 !px-[9px]  border border-white/10 z-[10] bg-blur menu-shadow text-white running-text-mono rounded-[16px] !gap-y-2">
+                <DropdownMenuItem className="flex !p-0  !my-0 w-full focus:bg-transparent focus:text-white  transition-all duration-300 ease-linear cursor-pointer">
                   <CustomButton
                     withIcon={true}
                     variant={"upgrade"}
                     className={"w-full"}
                   >
-                    <Diamond className='h-5 w-5' fill='white' />
+                    <Diamond className="h-5 w-5" fill="white" />
                     Upgrade
                   </CustomButton>
                 </DropdownMenuItem>
-                <DropdownMenuItem className='flex !p-0  !my-0 w-full focus:bg-transparent focus:text-white  transition-all duration-300 ease-linear cursor-pointer'>
+                <DropdownMenuItem className="flex !p-0  !my-0 w-full focus:bg-transparent focus:text-white  transition-all duration-300 ease-linear cursor-pointer">
                   <CustomMenuItem onClick={handleSaveCharacter}>
-                    <Save className='h-5 w-5' fill='white' />
+                    <Save className="h-5 w-5" fill="white" />
                     <span>Save Character</span>
                   </CustomMenuItem>
                 </DropdownMenuItem>
-                <DropdownMenuItem className='flex !p-0  !my-0 w-full focus:bg-transparent focus:text-white  transition-all duration-300 ease-linear cursor-pointer'>
+                <DropdownMenuItem className="flex !p-0  !my-0 w-full focus:bg-transparent focus:text-white  transition-all duration-300 ease-linear cursor-pointer">
                   <CustomMenuItem onClick={() => router.push("/")}>
-                    <Exit className='h-5 w-5' fill='white' />
+                    <Exit className="h-5 w-5" fill="white" />
                     <span>Exit Game</span>
                   </CustomMenuItem>
                 </DropdownMenuItem>
