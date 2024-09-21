96e4966d40e39a682d221003975e0e3fe8c44ee1
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-09-21 02:06:30 -0700

    revisions

--- components/navigation/GameplayNavbar.jsx
+++ components/navigation/GameplayNavbar.jsx
@@ -36,7 +36,6 @@ export default function GameplayNavbar({ loading, setLoading }) {
 
   const { setCurrentCharacter, game, currentCharacter } = useGameStore();
 
-  //saving character
   const handleSaveCharacter = async () => {
     try {
       setLoading(true);
